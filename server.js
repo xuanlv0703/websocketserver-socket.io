ca52fba43c572eb9f44f389ab620ed9c5758b8f0
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2015-12-29 13:33:48 +0000

    no message

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ var io = require('socket.io')(server);
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 server.listen( port, ipaddress, function() {
-    console.log((new Date()) + ' Server is listening on port 8080');
+    console.log((new Date()) + ' Server is listening on port ' + port);
 });
 
 
