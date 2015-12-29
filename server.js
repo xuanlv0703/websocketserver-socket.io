492b601fceecc55da5d2f61849a4acffe894fad1
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2015-12-29 13:10:42 +0000

    -

--- server.js
+++ server.js
@@ -5,11 +5,13 @@ var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
-var port = process.env.PORT || 8080;
 
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port 8080');
 });
+
 
 // Routing
 app.use(express.static(__dirname + '/public'));
