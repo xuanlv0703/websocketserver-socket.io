0f6e30f08989008f08a95ff91d165299fa97760f
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2016-01-17 10:44:45 +0000

    simplifying node web socket server

--- server.js
+++ server.js
@@ -1,20 +1,9 @@
 #!/bin/env node
 
-// Setup basic express server
-var express = require('express');
-var app = express();
-var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-server.listen( port, ipaddress, function() {
-    console.log((new Date()) + ' Server is listening on port ' + port);
-});
-
-
-// Routing
-app.use(express.static(__dirname + '/public'));
 
 // Chatroom
 
