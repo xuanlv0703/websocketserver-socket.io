f562949b4bb1f0c2980f756e9fbf855d84dc17f1
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2016-01-17 10:58:42 +0000

    no message

--- server.js
+++ server.js
@@ -4,8 +4,12 @@
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8880;
-
-var io = require('socket.io')(port);
+var io;
+try {
+	io = require('socket.io')(port);
+	} catch (err) {
+	console.log(err);
+	}
 // Chatroom
 
 var numUsers = 0;
