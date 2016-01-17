6e2eab8a13e040d3609bb0c4a0bcc5c522030900
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2016-01-17 10:51:12 +0000

    change port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
-var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8880;
 
 var io = require('socket.io')(port);
 // Chatroom
