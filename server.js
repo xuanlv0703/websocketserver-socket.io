e57df6a5fd447f829ca5de808bf48d203e80cb74
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2016-01-17 10:47:08 +0000

    fixed socket.io instantiation.

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 #!/bin/env node
 
-var io = require('socket.io')(server);
+
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 
+var io = require('socket.io')(port);
 // Chatroom
 
 var numUsers = 0;
