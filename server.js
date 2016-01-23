c21b9f48be619012e0706f79b9b7382d93cc7e15
Jorge C. S. Cardoso <jorgecardoso@ieee.org>
2016-01-23 14:23:11 +0000

    added access-control-allow headers

--- server.js
+++ server.js
@@ -3,6 +3,27 @@
 // Setup basic express server
 var express = require('express');
 var app = express();
+
+// Add headers
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', false);
+
+    // Pass to next layer of middleware
+    next();
+});
+
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
